8838aad3eddc76b70424f650bab3e3f39cadd185
thampiotr <17101802+thampiotr@users.noreply.github.com>
2017-02-17 08:59:54 +0000

    Initial check-in.

---
+++ src/piotrrr/collections/DoublyLinkedMap.java
@@ -0,0 +1,159 @@
+package piotrrr.collections;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+/**
+ * - Only small subset of operations we need.
+ * - Not thread safe.
+ * TODO:
+ * - Concurrent modification detection
+ * - TEST TEST TEST
+ * @param <K>
+ * @param <V>
+ */
+public class DoublyLinkedMap<K, V> {
+    private final HashMap<K, DoublyLinkedList.Node<V>> map = new HashMap<>();
+    private final DoublyLinkedList<V> list = new DoublyLinkedList<>();
+
+    public Optional<V> get(K key) {
+        DoublyLinkedList.Node<V> node = map.get(key);
+        return node == null ? Optional.empty() : Optional.of(node.value);
+    }
+
+    public Optional<V> putBack(K key, V value) {
+        Optional<V> previous = get(key);
+        DoublyLinkedList.Node<V> node = list.pushBack(value);
+        map.put(key, node);
+        return previous;
+    }
+
+    public Optional<V> putFront(K key, V value) {
+        Optional<V> previous = get(key);
+        DoublyLinkedList.Node<V> node = list.pushFront(value);
+        map.put(key, node);
+        return previous;
+    }
+
+    public Optional<V> remove(K key) {
+        DoublyLinkedList.Node<V> node = map.get(key);
+        if (node == null) {
+            return Optional.empty();
+        } else {
+            list.remove(node);
+            map.remove(key);
+            return Optional.of(node.value);
+        }
+    }
+
+    public Iterator<V> reverseValuesIterator() {
+        return list.reverseValuesIterator();
+    }
+
+    public Iterator<V> valuesIterator() {
+        return list.valuesIterator();
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    private static class DoublyLinkedList<V> {
+        private Node<V> head = null;
+        private Node<V> tail = null;
+
+        private Node<V> pushBack(V value) {
+            Node<V> toAdd = new Node<>(value);
+            toAdd.value = value;
+            if (head == null) { // in fact, both head and tail are null
+                head = toAdd;
+                tail = toAdd;
+            } else {
+                tail.next = toAdd;
+                toAdd.prev = tail;
+                tail = toAdd;
+            }
+            return toAdd;
+        }
+
+        private Node<V> pushFront(V value) {
+            Node<V> toAdd = new Node<>(value);
+            if (head == null) { // in fact, both head and tail are null
+                head = toAdd;
+                tail = toAdd;
+            } else {
+                head.prev = toAdd;
+                toAdd.next = head;
+                head = toAdd;
+            }
+            return toAdd;
+        }
+
+        /**
+         * @param node - a Node to remove. Cannot be null.
+         */
+        private void remove(Node<V> node) {
+            if (node == head) {
+                head = node.next;
+            }
+            if (node == tail) {
+                tail = node.prev;
+            }
+            if (node.next != null) {
+                node.next.prev = node.prev;
+            }
+            if (node.prev != null) {
+                node.prev.next = node.next;
+            }
+        }
+
+        private Iterator<V> reverseValuesIterator() {
+            return new ListIterator<>(this.tail, false);
+        }
+
+        private Iterator<V> valuesIterator() {
+            return new ListIterator<>(this.head, true);
+        }
+
+        private static class ListIterator<V> implements Iterator<V> {
+            private final boolean isForward;
+            private Node<V> position;
+
+            private ListIterator(Node<V> initialNode, boolean isForward) {
+                this.position = initialNode;
+                this.isForward = isForward;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return position != null;
+            }
+
+            @Override
+            public V next() {
+                if (position == null) {
+                    throw new NoSuchElementException("The list is empty.");
+                }
+                V value = position.value;
+                if (isForward) {
+                    position = position.next;
+                } else {
+                    position = position.prev;
+                }
+                return value;
+            }
+        }
+
+        private static class Node<V> {
+            private V value;
+            private Node<V> next = null;
+            private Node<V> prev = null;
+
+            public Node(V value) {
+                this.value = value;
+            }
+        }
+    }
+}
---
+++ test/piotrrr/collections/DoublyLinkedMapTest.java
@@ -0,0 +1,163 @@
+package piotrrr.collections;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DoublyLinkedMapTest {
+
+    @Test
+    public void basicPutAndGet() throws Exception {
+        int testSize = 100;
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.get("1").isPresent());
+        Assert.assertEquals(0, map.size());
+        for (int i = 0; i < testSize; i++) {
+            if (i % 2 == 0) {
+                map.putBack(String.valueOf(i), i * i);
+            } else {
+                map.putFront(String.valueOf(i), i * i);
+            }
+        }
+        Assert.assertFalse(map.get("123").isPresent());
+        for (int i = 0; i < testSize; i++) {
+            Assert.assertEquals(Integer.valueOf(i * i), map.get(String.valueOf(i)).orElse(-1));
+        }
+        Assert.assertEquals(testSize, map.size());
+    }
+
+    @Test
+    public void iteratesForwardWithPutBack() throws Exception {
+        int testSize = 100;
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.get("1").isPresent());
+        for (int i = 0; i < testSize; i++) {
+            map.putBack(String.valueOf(i), i * i);
+        }
+        Iterator<Integer> it = map.valuesIterator();
+        for (int i = 0; i < testSize; i++) {
+            Assert.assertTrue(it.hasNext());
+            Assert.assertEquals(Integer.valueOf(i * i), it.next());
+        }
+    }
+
+    @Test
+    public void iteratesForwardPutFront() throws Exception {
+        int testSize = 100;
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.get("1").isPresent());
+        for (int i = 0; i < testSize; i++) {
+            map.putFront(String.valueOf(i), i * i);
+        }
+        Iterator<Integer> it = map.valuesIterator();
+        for (int i = 0; i < testSize; i++) {
+            Assert.assertTrue(it.hasNext());
+            int expected = testSize - i - 1;
+            expected = expected * expected;
+            Assert.assertEquals(Integer.valueOf(expected), it.next());
+        }
+    }
+
+    @Test
+    public void iteratesReversePutBack() throws Exception {
+        int testSize = 100;
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.get("1").isPresent());
+        for (int i = 0; i < testSize; i++) {
+            map.putBack(String.valueOf(i), i * i);
+        }
+        Iterator<Integer> it = map.reverseValuesIterator();
+        for (int i = 0; i < testSize; i++) {
+            Assert.assertTrue(it.hasNext());
+            int expected = testSize - i - 1;
+            expected = expected * expected;
+            Assert.assertEquals(Integer.valueOf(expected), it.next());
+        }
+    }
+
+    @Test
+    public void iteratesReversePutFront() throws Exception {
+        int testSize = 100;
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.get("1").isPresent());
+        for (int i = 0; i < testSize; i++) {
+            map.putFront(String.valueOf(i), i * i);
+        }
+        Iterator<Integer> it = map.reverseValuesIterator();
+        for (int i = 0; i < testSize; i++) {
+            Assert.assertTrue(it.hasNext());
+            Assert.assertEquals(Integer.valueOf(i * i), it.next());
+        }
+    }
+
+    @Test
+    public void removal() throws Exception {
+        int testSize = 100;
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertEquals(0, map.size());
+        for (int i = 0; i < testSize; i++) {
+            map.putBack(String.valueOf(i), i * i);
+        }
+        Assert.assertEquals(testSize, map.size());
+        for (int i = 0; i < testSize; i += 2) {
+            map.remove(String.valueOf(i));
+        }
+        Assert.assertEquals(testSize/2, map.size());
+        for (int i = 0; i < testSize; i += 2) {
+            Assert.assertFalse(map.get(String.valueOf(i)).isPresent());
+        }
+        for (int i = 1; i < testSize; i += 2) {
+            Assert.assertTrue(map.get(String.valueOf(i)).isPresent());
+        }
+        Iterator<Integer> it = map.valuesIterator();
+        for (int i = 1; i < testSize; i += 2) {
+            Assert.assertEquals(Integer.valueOf(i * i), it.next());
+        }
+    }
+
+    @Test
+    public void emptyMap() throws Exception {
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.remove("abc").isPresent());
+        Assert.assertFalse(map.get("sth").isPresent());
+        Assert.assertFalse(map.valuesIterator().hasNext());
+        Assert.assertFalse(map.reverseValuesIterator().hasNext());
+    }
+
+    @Test
+    public void addAndRemoveOne() throws Exception {
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        Assert.assertFalse(map.get("sth").isPresent());
+        Assert.assertFalse(map.valuesIterator().hasNext());
+        Assert.assertFalse(map.reverseValuesIterator().hasNext());
+        Assert.assertEquals(0, map.size());
+
+        map.putBack("sth", 123);
+        Assert.assertEquals(Integer.valueOf(123), map.get("sth").get());
+        Assert.assertEquals(1, map.size());
+
+        Iterator<Integer> fwdIt = map.valuesIterator();
+        Assert.assertTrue(fwdIt.hasNext());
+        Assert.assertEquals(Integer.valueOf(123), fwdIt.next());
+        Assert.assertFalse(fwdIt.hasNext());
+
+        Iterator<Integer> revIt = map.reverseValuesIterator();
+        Assert.assertTrue(revIt.hasNext());
+        Assert.assertEquals(Integer.valueOf(123), revIt.next());
+        Assert.assertFalse(revIt.hasNext());
+
+        map.remove("sth");
+        Assert.assertEquals(0, map.size());
+        Assert.assertFalse(map.get("sth").isPresent());
+        Assert.assertFalse(map.valuesIterator().hasNext());
+        Assert.assertFalse(map.reverseValuesIterator().hasNext());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void throwsExceptionWhenIteratorInvalid() throws Exception {
+        DoublyLinkedMap<String, Integer> map = new DoublyLinkedMap<>();
+        map.reverseValuesIterator().next();
+    }
+}
